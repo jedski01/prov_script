d5082daa7af5681561417e40cf9fc5c88e0d17d2
jedski01 <jedikin@gmail.com>
2018-02-14 13:17:44 -0800

    added the web_service_env_vm.sh

--- pxe_kickstart/web_service_env_vm.sh
+++ pxe_kickstart/web_service_env_vm.sh
@@ -4,6 +4,7 @@ vboxmanage () { VBoxManage.exe "$@"; }
 
 scp wp_ks.cfg pxe:/usr/share/nginx/html
 scp -r ../scripted_config/wp_config_files pxe:/usr/share/nginx/html/
+scp wp_mariadb_config.service wp_mariadb_config.sh pxe:/usr/share/nginx/html/wp_config_files
 ssh pxe 'sudo chown nginx:wheel /usr/share/nginx/html/wp_ks.cfg'
 ssh pxe 'chmod ugo+r /usr/share/nginx/html/wp_ks.cfg'
 ssh pxe 'chmod ugo+rx /usr/share/nginx/html/wp_config_files'
---
+++ pxe_kickstart/wp_mariadb_config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#log to journal showing script start
+systemd-cat -p "notice" -t wp_mariadb_config printf "%s" "wp_mariadb_config.sh start" 
+
+#execute wp_mariadb_config.sql statements as the root mysql user, 
+mysql -u root < mariadb_security_config.sql
+systemctl restart mariadb 
+mysql -u root -pP@ssw0rd < wp_mariadb_config.sql
+
+#remember password for root hasn't been set yet
+
+#Disable the wp_mariadb_config.service
+sytemctl disable wp_mariadb_config.service
+
+#log to journal showing script end
+systemd-cat -p "notice" -t wp_mariadb_config printf "%s" "wp_mariadb_config.sh end" 
