99d2882771f90be7e23822c7f97e50e125d30736
jedski01 <jiquin1@my.bcit.ca>
2018-02-15 12:40:47 -0800

    added changes to change ownership and permission of nginxx/html

--- pxe_kickstart/web_service_env_vm.sh
+++ pxe_kickstart/web_service_env_vm.sh
@@ -11,10 +11,12 @@ until [[ $(ssh -q pxe exit && echo "online") == "online" ]] ; do
   echo "waiting for pxe server vm to come online"
 done
 
+ssh pxe 'sudo chown -R nginx:wheel /usr/share/nginx/html'
+ssh pxe 'sudo chmod -R ug+w /usr/share/nginx/html'
+
 scp wp_ks.cfg pxe:/usr/share/nginx/html
 scp -r ../scripted_config/wp_config_files pxe:/usr/share/nginx/html/
 scp wp_mariadb_config.service wp_mariadb_config.sh pxe:/usr/share/nginx/html/wp_config_files
-ssh pxe 'sudo chown nginx:wheel /usr/share/nginx/html/wp_ks.cfg'
 ssh pxe 'sudo chmod ugo+r /usr/share/nginx/html/wp_ks.cfg'
 ssh pxe 'chmod ugo+rx /usr/share/nginx/html/wp_config_files'
 ssh pxe 'chmod -R ugo+r /usr/share/nginx/html/wp_config_files/*'
